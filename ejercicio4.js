e98c383dafb4e74d4f9969fd69b7975b56b9f231
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-29 08:46:35 -0300

    ej. IF 4 c/lbl&inp v1

---
+++ ejercicio4.js
@@ -0,0 +1,52 @@
+/*
+Programamos a un Robot en preparar Sopas.
+Si hay Verduras, Carne ó Pollo, Agua y Condimentos 
+En caso no haya Condimentos pero Sí un Cubo de Caldo,
+entonces se prepara igual...
+Además informa el resultado...  
+*/
+/* SE ASUME QUE EL ROBOT CUENTA CON AGUA DESDE UNA CONEXIÓN DIRECTA
+   Y QUE SE LE DEBEN AGREGAR LOS DEMÁS INGREDIENTES
+*/
+
+/*
+En la Versión 2 uso la función "confirm()"
+*/
+
+const quiereSopa        = document.querySelector ("#lbl-sop")
+const hayVerduras       = document.querySelector ("#lbl-ver")
+const hayCarne          = document.querySelector ("#lbl-car")
+const hayPollo          = document.querySelector ("#lbl-poll")
+const hayCondimentos    = document.querySelector ("#lbl-cond")
+const hayCuboDeCaldo    = document.querySelector ("#lbl-cuca")
+const miSalida          = document.querySelector ("#mi-salida")
+
+const SI                = "s" ;
+
+function verificaSopa() {
+
+if (!(quiereSopa.value == SI)) {
+    miSalida.style.color = "violet" ;
+    miSalida.textContent = "OK! será en otra oportunidad...";
+} 
+else {
+      if ((hayVerduras.value=SI)&&((hayCarne.value==SI)||(hayPollo.value==SI))&&(hayCondimentos.value==SI)){
+        miSalida.style.color = "green" ;
+        miSalida.textContent = "Hay SOPA con Carne o Pollo mas Condimentos";            
+       }
+       else if ((hayVerduras.value==SI) &&((hayCarne.value==SI) ||(hayPollo.value==SI ))&&(hayCuboDeCaldo.value==SI)){
+        miSalida.style.color = "blue" ;
+        miSalida.textContent = "Hay SOPA con Carne o Pollo y Cubo de Caldo";
+      }
+      else {
+      miSalida.style.color = "red" ;
+      miSalida.textContent = "Lo siento FALTAN INGREDIENTES...";
+      }
+
+    };
+
+}
+
+
+
+
